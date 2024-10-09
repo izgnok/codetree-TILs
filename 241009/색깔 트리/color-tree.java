c1e64d8977af62fa838fae9c246497d384d34aff
공지환 <101163901+izgnok@users.noreply.github.com>
2024-10-09 19:49:26 +0900

    Title: 색깔 트리, Time: 125ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 241009/색깔 트리/color-tree.java
@@ -0,0 +1,129 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static int N;
+	static Node[] nodeList;
+	static boolean[] checkColor;
+
+	public static void main(String[] args) throws IOException {
+		// 여기에 코드를 작성해주세요.
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		N = Integer.parseInt(st.nextToken());
+		nodeList = new Node[100001];
+
+		for(int i=1; i<=N; i++) {
+			
+			st = new StringTokenizer(br.readLine());
+			int K = Integer.parseInt(st.nextToken());
+
+			if (K == 100) {
+				// 삽입
+				int id = Integer.parseInt(st.nextToken());
+				int parent_id = Integer.parseInt(st.nextToken());
+				int color = Integer.parseInt(st.nextToken());
+				int own_depth = Integer.parseInt(st.nextToken());
+
+				if (parent_id == -1) {
+					nodeList[id] = new Node(parent_id, color, own_depth, own_depth, i);
+				} else {
+					Node node = nodeList[parent_id];
+					if (node == null)
+						continue;
+					if (node.own_depth == 1 || node.total_depth == 1)
+						continue;
+
+					int total_depth = Math.max(node.own_depth, node.total_depth) - 1;
+					nodeList[id] = new Node(parent_id, color, own_depth, total_depth, i);
+					nodeList[parent_id].child.add(id);
+				}
+			} else if (K == 200) {
+				// 색깔 변경
+				int id = Integer.parseInt(st.nextToken());
+				int color = Integer.parseInt(st.nextToken());
+				Node node = nodeList[id];
+				if(node == null) continue;
+				node.color = color;
+				node.last_update = i;
+			} else if (K == 300) {
+				// 색깔 조회
+				int id = Integer.parseInt(st.nextToken());
+				int info[] = getColor(nodeList[id]);
+				sb.append(info[0]).append("\n");
+			} else if (K == 400) {
+				// 가치 조회
+				int sum = 0;
+				for(int j=1; j<= 100000; j++) {
+					Node node = nodeList[j];
+					if(node == null) continue;
+					checkColor = new boolean[6];
+					getSum(node, node.color, node.last_update);
+					
+					int cnt = 0;
+					for(int t=1; t<=5; t++) {
+						if(checkColor[t]) cnt++;
+					}
+					sum += cnt * cnt;
+				}
+				sb.append(sum).append("\n");
+			}
+		}
+
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+	}
+	
+	public static void getSum(Node node, int color, int last_update) {
+		
+		if(last_update < node.last_update) {
+			last_update = node.last_update;
+			color = node.color;
+		}
+		checkColor[color] = true;	
+		List<Integer> childs = node.child;
+		for(int i=0; i<childs.size(); i++) {
+			Node child = nodeList[childs.get(i)];
+			getSum(child, color, last_update);
+		}
+	}
+	
+	public static int[] getColor(Node node)  {
+		if(node.parent_id == -1) {
+			return new int[] { node.color, node.last_update };
+		}
+		int[] info = getColor(nodeList[node.parent_id]);
+		if(info[1] > node.last_update) {
+			return info;
+		}
+		else {
+			return new int[] { node.color, node.last_update };
+		}
+	}
+
+	public static class Node {
+		int parent_id, color, own_depth, total_depth, last_update;
+		List<Integer> child;
+
+		Node(int parent_id, int color, int own_depth, int total_depth, int last_update) {
+			this.parent_id = parent_id;
+			this.color = color;
+			this.own_depth = own_depth;
+			this.total_depth = total_depth;
+			this.last_update = last_update;
+			this.child = new ArrayList<>();
+		}
+
+		@Override
+		public String toString() {
+			return "Node [parent_id=" + parent_id + ", color=" + color + ", own_depth=" + own_depth + ", total_depth="
+					+ total_depth + ", last_update=" + last_update + ", child=" + child + "]";
+		}
+		
+		
+	}
+}
